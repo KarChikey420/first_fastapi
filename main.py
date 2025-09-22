e0c8b1fa7ac8dbab930c740b9917eeafceb81833
KarChikey <kartikeynegi2002@gmail.com>
2025-09-22 19:26:06 +0530

    post

--- main.py
+++ main.py
@@ -13,6 +13,19 @@ product_list = [
     product(id=3, name="ghi", classes="class3")
 ]
 
-@app.get("/product")
+@app.get("/products")
 def get_all_product():
     return product_list
+
+@app.get("/product/{id}")
+def get_one_product(id:int):
+    for product in product_list:
+       if product.id== id:
+           return product
+       
+    return {"meassage":"not in list"}
+
+@app.post("/product")
+def add_product(product:product):
+    product_list.append(product)
+    return product
