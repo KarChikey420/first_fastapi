e22fbc5b50313a5bf1034bbc928fbfebea7e4e14
KarChikey <kartikeynegi2002@gmail.com>
2025-09-25 11:34:10 +0530

    start

---
+++ Crud_Operation/crud.py
@@ -0,0 +1,13 @@
+import uvicorn
+from fastapi import FastAPI
+from sqlalchemy import Integer,String, create_engine,column
+from sqlalchemy.ext.declarative import declarative_base 
+from sqlalchemy.orm import sessionmaker
+
+SqolAlchemy_URL = "sqlite:///./crud.db"
+engine=create_engine(SqolAlchemy_URL)
+SessionLocal=sessionmaker(autocommit=False,autoflush=False,bind=engine)
+Base=declarative_base()
+
+
+    
