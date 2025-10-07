3849ea41afc983b67dca00dd647ccd30ac821720
KarChikey <kartikeynegi2002@gmail.com>
2025-10-07 18:22:41 +0530

    add

---
+++ auth_app/model.py
@@ -0,0 +1,11 @@
+from sqlalchemy import Column,Integer,String
+from .database import Base
+
+class Student(Base):
+    __tablename__="users"
+    id=Column(Integer,primary_key=True,index=True)
+    user_name=Column(String(20),unique=True,nullable=False,index=True)
+    email=Column(String(20),unique=True,nullable=False,index=True)
+    password=Column(String)
+    
+    
