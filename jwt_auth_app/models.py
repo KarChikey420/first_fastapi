7b1b7fb756b336efb431432c8030808bbabb16a5
KarChikey <kartikeynegi2002@gmail.com>
2026-02-17 23:19:35 +0530

    add

--- jwt_auth_app/models.py
+++ jwt_auth_app/models.py
@@ -2,7 +2,7 @@ from sqlalchemy import Integer,Column,String
 from .database import Base
 
 class User(Base):
-    __tablename__="user"
+    __tablename__="useres"
     id=Column(Integer,primary_key=True,index=True)
     username=Column(String(20),unique=True,nullable=False,index=True)
     email=Column(String(20), unique=True, nullable=False, index=True)
